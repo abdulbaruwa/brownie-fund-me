9adbc604a6a5f57eafff7429bdb4f163ddf3a376
Shangoman <abdulbaruwa@hotmail.com>
2021-10-01 08:08:29 +0100

    init fund me brownie project

---
+++ scripts/deploy.py
@@ -0,0 +1,10 @@
+from brownie import FundMe
+from scripts.helpful_scripts import get_account
+
+def deploy_fund_me():
+    account = get_account()    
+    #  pass the price feed address to our fundme contract
+    fund_me = FundMe.deploy({"from": account}, publish_source=True)
+    print(f"contract deployed to {fund_me.address}")
+def main():
+     deploy_fund_me()
